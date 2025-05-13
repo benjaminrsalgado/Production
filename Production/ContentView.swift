95190b9f8ba065d5df5b8511d203dc17f0384d9a
Benjamin Rojo <benjaminrsalgado@gmail.com>
2025-05-13 13:50:39 -0600

    i add the for each

--- Production/ContentView.swift
+++ Production/ContentView.swift
@@ -10,14 +10,58 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+           // here are the two views
+            ProductView()
+            repeticion()
         }
         .padding()
     }
 }
+
+//Estructura con id
+struct ProductView: View{
+    let productos = ["melon, sandia, papa, fresa, papaya"]
+    var body: some View{
+        VStack{
+            ForEach(productos, id: \.self){name in
+                    Text(name)
+                    .foregroundColor(.blue)
+                    .font(.title)
+            }
+        }
+    }
+}
+
+//Estructura sin id usando Identifiable and UUID()
+struct Perfiles: Identifiable {
+    let id = UUID()
+    let usuarios: String
+    let rol: String
+}
+let trabajadores = [
+    Perfiles(usuarios: "Benjamin", rol: "iOS Developer"),
+    Perfiles(usuarios: "Frank", rol: "iOS Developer"),
+    Perfiles(usuarios: "Taylor", rol: "iOS Developer"),
+    Perfiles(usuarios: "Selena", rol: "iOS Developer")
+]
+
+struct repeticion: View{
+    var body:some View{
+        VStack{
+            ForEach(trabajadores){name in
+                HStack{
+                    Text(name.usuarios)
+                    Text(name.rol)
+
+                }
+                .foregroundColor(.red)
+                .font(.headline)
+            }
+        }
+    }
+}
+
+ 
 
 #Preview {
     ContentView()
