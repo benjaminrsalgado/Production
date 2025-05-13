0a2d2546dc4a9849a6d7cb7a55988b3d882abd6c
Benjamin Rojo <benjaminrsalgado@gmail.com>
2025-05-13 13:14:15 -0600

    Initial Commit

---
+++ Production/ProductionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductionApp.swift
+//  Production
+//
+//  Created by Benjamin Rojo on 13/05/25.
+//
+
+import SwiftUI
+
+@main
+struct ProductionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
